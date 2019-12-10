66210783082776caa5f1e95fb4695abba1a03fb5
Jonny Deates <48420377+JonnyDeates@users.noreply.github.com>
2019-12-10 12:10:09 -0700

    Updates

--- Big-O.js
+++ Big-O.js
@@ -1,13 +1,16 @@
-let disks = [[0, 1, 2], [], []];
-
-function towerofhanoi(disks) {
-    for (let disk of disks) {
-        if (disk[disk.length - 1]) {
-
-        }
+function TOH(disc,A,B,C, count){
+    if(disc > 0){
+        count[0] += 1;
+        TOH(disc -1,A,C,B, count);
+        console.log('Disc '+disc+' from '+A+' to '+C);
+        TOH(disc -1,B,A,C, count);
     }
+    return count[0];
 }
-
+''+''+''
+//console.log(TOH(3,'A','B','C',[0]));
+//console.log(TOH(4,'A','B','C',[0]));
+console.log(TOH(5,'A','B','C',[0]));
 function countingSheep(n) {
     for (let i = 0; i < n; i++) {
         console.log('Another Sheep Jumps Over the Fence');
@@ -26,7 +29,7 @@ function powerCalculator(base, exp) {
     return product;
 }
 
-console.log(powerCalculator(10, 10));
+//console.log(powerCalculator(10, 10));
 
 function reverseString(str){
     return str.reverse();
@@ -39,7 +42,7 @@ function trangularNumber(n){
     }
     return Sum;
 }
-console.log(trangularNumber(2000));
+//console.log(trangularNumber(2000));
 
 function stringSplitter(str,split){
     return str.split(split);
@@ -60,7 +63,7 @@ function compute(num) {
     }
     return result;
 }
-console.log(compute(1));
+//console.log(compute(1));
 function factorial(num){
     let product = 1;
     for(let i=1; i <= num;i++){
@@ -68,4 +71,4 @@ function factorial(num){
     }
     return product;
 }
-console.log(factorial(500));
+//console.log(factorial(500));
--- README.md
+++ README.md
@@ -11,7 +11,12 @@
 9)Constant - doesnt matter how big the array is.
 10)Check to see if it is greater than 2, and is not an interger. Then finds if it is prime. Linear.
 11)If you are given 5 disks, how do the rods look like after 7 recursive calls?
+    -          -      -
+    -          -     ---
+ --------      -    -----
+----------     -   -------    
 How many moves are needed to complete the puzzle with 3 disks? with 4 disks? with 5 disks?
+7,15,31
 Exponential - moves corrispond to the number of disks. 
 13-1) Linear
 13-2) Polynomial
